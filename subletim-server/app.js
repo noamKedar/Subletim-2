5ca294c078578aa5c4399b08c581f3977f9442d8
Noam Kedar <noam.kedar@amobee.com>
2020-06-06 16:22:19 +0300

    app listen

--- subletim-server/app.js
+++ subletim-server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./src/routes/index');
 
 const app = express();
 const cors = require('cors');
@@ -18,7 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/history', historyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,4 +33,8 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
+});
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
 });
